3b9b792e02648847d4876183453ff5c5fc608e0a
Scott Draves <spot@draves.org>
2015-06-12 02:31:12 +0000

    version to 3.1.0

--- config.h
+++ config.h
@@ -84,7 +84,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "3.0.2"
+#define VERSION "3.1.0"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
